ae9c134c47c7a2366374f7539f0e5d9c0ed08577
Zaid Seta <zaidmseta@gmail.com>
2026-02-23 13:32:04 -0500

    added experience info

--- src/data/experience.js
+++ src/data/experience.js
@@ -1,11 +1,24 @@
 export const experiences = [
+    /* Template
+    {
+      id: "",
+      title: "",
+      company: "",
+      logo: "/", 
+      location: "",
+      start: "",
+      end: "",
+      description: [
+        ""
+       ],
+    },
+    */
     {
       id: "sales-associate-1",
       title: "Sales Associate",
       company: "Bell Canada",
-      logo: "/", // tba
+      logo: "../public/logos/Bell_logo.png", 
       location: "Hamilton, ON",
-      type: "Part-time",
       start: "2025-05",
       end: "2025-12",
       description: [
@@ -14,6 +27,32 @@ export const experiences = [
         "Resolved customer issues quickly, reducing escalations and improving satisfaction.",
       ],
     },
-//TBA other roles
+    {
+      id: "camp-counsellor-1",
+      title: "Camp Counsellor",
+      company: "Focus on Youth",
+      logo: "../public/logos/FOY_logo.png", 
+      location: "Hamilton, ON",
+      start: "2023-06",
+      end: "2023-08",
+      description: [
+        "Facilitated daily educational and recreational activities for 15+ youth, promoting teamwork and creativity.",
+        "Organized and supervised STEM-themed activities, sparking interest in technology among campers."
+       ],
+    },
+    {
+      id: "store-associate-1",
+      title: "Store Associate",
+      company: "Krazy Binz",
+      logo: "../public/logos/KB_logo.jpg", 
+      location: "Hamilton, ON",
+      start: "2022-11",
+      end: "2023-06",
+      description: [
+        "Managed inventory restocking and maintained organized store layouts for efficient customer navigation.",
+        "Assisted customers with product inquiries, resolving issues to enhance shopping experiences.",
+       ],
+    },
+    
   ];
   
